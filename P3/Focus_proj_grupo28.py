271162f1b5b0248dea9ad15633b68d12eb3a20be
juniper <juniper.figueira@gmail.com>
2025-10-29 10:57:24 +0000

    documentation

--- P3/Focus_proj_grupo28.py
+++ P3/Focus_proj_grupo28.py
@@ -1,8 +1,36 @@
 from focus import EstadoFocus, JogoFocus
 from typing import Callable
+from collections import namedtuple
+from jogos import Game
 
+infinity = float('inf')
+
+# funcao do Basicus
+def func_basicus(estado,jogador) :
+    clone = copy.deepcopy(estado)
+    winner = clone.winner()
+    
+    # --- Caso terminal ---
+    if winner is not None:
+        return infinity if winner == jogador else -infinity
+
+    my_pilhas = clone.dominate_piles(jogador)
+    opponent = 'GREEN' if jogador == 'RED' else 'RED'
+    opp_pilhas = clone.dominate_piles(opponent)
+    return my_pilhas - opp_pilhas
+
+
+
+# Objetivo eh escrever uma funcao que maximiza a pontuacao no torneio
                                         # funcao que recebe JogoFocus, EstadoFocus retorna tuple descrito abaixo
 def func_28(estado: EstadoFocus, jogador: Callable[[JogoFocus, EstadoFocus], tuple]):
+    """
+    Estado do jogo Focus:
+    - 'board': dicionário {(x,y): [peças, de baixo para cima]}
+    - 'reserve': {'RED': n, 'GREEN': n}
+    - 'captured': {'RED': n, 'GREEN': n}
+    """
+    
     """
     Jogador retorna uma jogada/action/move que pode estar na formas:
     - ((x,y), direction) ex ((1,3), "up")   --  move da pos (1,3) para (1,2)
